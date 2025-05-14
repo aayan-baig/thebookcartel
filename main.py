8e2b52e3b5da6028736d6578208fa035717255aa
The-Anonymous-Developer <95202769+The-Anonymous-Developer@users.noreply.github.com>
2025-05-15 00:40:14 +0530

    A lot of development

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from flask import *
 
 app = Flask(__name__)
+app.secret_key = '26102705'
 
 @app.route('/')
-def homepage():
-    #ill use jinja2 templates idrk lmao
+def index():
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        message = request.form.get('message')
+        flash('Thanks for contacting us! You will receive a mail from our outreach department as soon as possible.', 'success')
+        return redirect(url_for('contact'))
+
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
